dcd877233d9d1a5828c0c1fce364d7882b6321a1
Pedro Lima <pedrofelixcfn@gmail.com>
2021-02-03 00:04:55 -0300

    componente cadastro lancamento criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
 
 
 
@@ -14,20 +16,25 @@ import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule
 
   ],
   providers: [],
--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -9,31 +9,31 @@ export class LancamentosPesquisaComponent {
 
   lancamentos: any = [
 
-    { tipo: 'DESPESA', pessoa: 'Zezinho da silva', descricao: 'Compra de pão', vencimento: '23/12/2020', pagamento: '233',
-      valor: '255'},
+    { tipo: 'DESPESA', pessoa: 'Zezinho da silva', descricao: 'Compra de pão', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
+      valor: '255.55'},
 
-    { tipo: 'DESPESA', pessoa: 'Fulaninho costa', descricao: 'Compra de queijos', vencimento: '23/12/2020', pagamento: '432',
+    { tipo: 'DESPESA', pessoa: 'Fulaninho costa', descricao: 'Compra de queijos', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '746'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'},
 
-    { tipo: 'DESPESA', pessoa: 'Beltraninho moraes', descricao: 'Compra de maionese', vencimento: '23/12/2020', pagamento: '153',
+    { tipo: 'DESPESA', pessoa: 'Beltraninho moraes', descricao: 'Compra de maionese', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '632'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'},
 
-    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: '23/12/2020', pagamento: '421',
+    { tipo: 'RECEITA', pessoa: 'Sicraninho bezerra', descricao: 'Compra de presunto', vencimento: new Date(2020, 11, 29), pagamento: new Date(2020, 12, 29),
       valor: '321'}
 
 
